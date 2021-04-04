15ac4a1ebfe53fe7485cae53f66ceda86eeaa0a4
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-04-04 03:09:04 +0200

    Added ServerMain class

---
+++ AccountingOfficeManagerServer/src/main/java/ServerMain.java
@@ -0,0 +1,35 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ServerMain {
+
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello World");
+        createServer();
+    }
+
+    private static void createServer(){
+        int port = 3999;
+        try{
+            // Create HttpServer
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+//            server.createContext("/", new HandlerHttpHandler());
+            server.setExecutor(threadPoolExecutor);
+
+            //Start HttpServer
+            server.start();
+            System.out.println("Server started on port " + port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
