72bbdb2d5464a27f17ad7b31a647473266317b9c
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-04 16:00:54 +0200

    Added Documents package

---
+++ src/main/java/com/example/AccountingOfficeManagerServer/Entity/Documents/DocumentController.java
@@ -0,0 +1,53 @@
+package com.example.AccountingOfficeManagerServer.Entity.Documents;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/document")
+public class DocumentController {
+    @Autowired
+    DocumentService documentService;
+
+    @GetMapping("")
+    public List<Document> list() {
+        return documentService.listAllDocument();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Document> get(@PathVariable Integer id) {
+        try {
+            Document document = documentService.getDocument(id);
+            return new ResponseEntity<Document>(document, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Document>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/")
+    public void add(@RequestBody Document document) {
+        documentService.saveDocument(document);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Document document, @PathVariable Integer id) {
+        try {
+            Document existDocument = documentService.getDocument(id);
+//            Document.setId(id);
+            documentService.saveDocument(document);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        documentService.deleteDocument(id);
+    }
+}
---
+++ src/main/java/com/example/AccountingOfficeManagerServer/Entity/Documents/DocumentRepository.java
@@ -0,0 +1,6 @@
+package com.example.AccountingOfficeManagerServer.Entity.Documents;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentRepository extends JpaRepository<Document, Integer>{
+}
