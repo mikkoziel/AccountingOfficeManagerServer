3898c5d8c7e13d3d278cc3fdf41d108fad22e07c
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-18 14:14:17 +0200

    Changed query in ClientRepo

--- src/main/java/com/example/AccountingOfficeManagerServer/repository/ClientRepository.java
+++ src/main/java/com/example/AccountingOfficeManagerServer/repository/ClientRepository.java
@@ -13,8 +13,9 @@ public interface ClientRepository extends JpaRepository<Client, Integer>{
     @Query(value = "SELECT * FROM client" +
             " JOIN user" +
             " ON client.user_id=user.user_id" +
+            " AND client.employee_id=?1" +
             " JOIN company" +
-            " ON user.company_id=company.company_id" +
-            " AND client.employee_id=?1", nativeQuery = true)
+            " ON user.company_id=company.company_id"
+            , nativeQuery = true)
     List<Client> findByEmployeeId(Integer user_id);
 }
