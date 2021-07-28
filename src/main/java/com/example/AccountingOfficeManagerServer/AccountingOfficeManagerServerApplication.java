9566996087ba737955a65a3c005c792bc1e48120
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-29 00:47:38 +0200

    Added bean for init of file storage

--- src/main/java/com/example/AccountingOfficeManagerServer/AccountingOfficeManagerServerApplication.java
+++ src/main/java/com/example/AccountingOfficeManagerServer/AccountingOfficeManagerServerApplication.java
@@ -1,14 +1,26 @@
 package com.example.AccountingOfficeManagerServer;
 
+import com.example.AccountingOfficeManagerServer.entity.configuration.StorageProperties;
+import com.example.AccountingOfficeManagerServer.service.DocumentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class AccountingOfficeManagerServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountingOfficeManagerServerApplication.class, args);
 	}
 
-
+	@Bean
+	CommandLineRunner init(DocumentService storageService) {
+		return (args) -> {
+//			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
